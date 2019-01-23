ada69502d655ce14da5eb6a56b804670d7530f77
kennethcurrie <kjcurrie@comcast.net>
2019-01-23 16:53:54 -0500

    Update user.router.ts
    changed patch to post, as html forms do not allow patch

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -42,20 +42,26 @@ userRouter.get('', (req, res) => {
     res.status(200).send(pageGenerator(['users', userTable(users, req.session.user.role)], req.session.user.role));
 });
 
+// patch user
+userRouter.post('*', (req, res) => {
+  console.log('patch, I guess');
+  res.status(200).send(pageGenerator(['users', userTable(users, req.session.user.role)], req.session.user.role));
+});
+
 // change ?id=# to /#
 userRouter.get('', (req, res, next) => {
   console.log(req.params);
   if (req.query.id === undefined) {
     next();
   }
-  res.redirect('/users/' + req.query.id);
+  res.redirect('' + req.query.id);
   next();
 });
 
 function userTable(users, role) {
   let data = '';
   if (users.constructor == Array) {
-    data += `<form action="users" method="get">Select user by ID: <input type="number" name="id"><input type="submit"></form>`;
+    data += `<form action="users" method="patch">Select user by ID: <input type="number" name="id"><input type="submit"></form>`;
   }
   data += `<Table><tr>
   <td>ID</td>
@@ -74,34 +80,34 @@ function userTable(users, role) {
       <td>${element.firstName}</td>
       <td>${element.lastName}</td>
       <td><a href="mailto:${element.email}">${element.email}</a></td>
-      <td>${element.role}</td>
+      <td>${element.role.role}</td>
       </tr>`;
     });
   } else {
-    data += `<form action="users" method="patch">
+    data += `<form action="" method="post">
     <tr>
-    <td><input name="userId" type="hidden">${users.userId}</input></td>
+    <td><input name="userId" type="hidden" value="${users.userId}">&nbsp;</input></td>
     <td><input name="username" value="${users.username}"></input></td>
     <td><input name="password" value=""></input></td>
     <td><input name="firstName" value="${users.firstName}"></input></td>
     <td><input name="lastName" value="${users.lastName}"></input></td>
     <td><input name="email" value="${users.email}"></input></td>
     <td><select name="role">`;
-    switch (users.role) {
+    switch (users.role.role) {
       case 'admin':
-        data += `<option value="admin" selected>admin</option>
+        data += `<option value="admin" selected="true">admin</option>
         <option value="finance">finance</option>
-        <option value="user">user</option>`;
+        <option value="associate">associate</option>`;
       break;
-      case 'finance':
+      case 'finance-manager':
         data += `<option value="admin">admin</option>
-        <option value="finance" selected>finance</option>
-        <option value="user">user</option>`;
+        <option value="finance-manager" selected="true">finance-manager</option>
+        <option value="associate">associate</option>`;
       break;
       default:
         data += `<option value="admin">admin</option>
         <option value="finance">finance</option>
-        <option value="user" selected>user</option>`;
+        <option value="associate" selected="true">associate</option>`;
       break;
     }
     data += `</select></td>
